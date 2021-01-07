02c23b0b10c321322dd503164d88025b08b17610
Hector Herrera <hectorsum81@hotmail.com>
2021-01-07 10:58:05 -0500

    index.css and index.js files were updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import { Root } from './components/Root/Root';
+import * as theme from './config/theme';
 import "./index.css";
-ReactDOM.render(<Root/>,document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}> 
+    <Root/>
+  </ThemeProvider>
+  ,
+  document.getElementById('root'));
