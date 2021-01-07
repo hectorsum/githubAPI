9b56bc631bce39a0f9c8badeec64fa794b4e5bb4
Hector Herrera <hectorsum81@hotmail.com>
2021-01-07 16:27:30 -0500

    theme updated

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,7 +1,4 @@
-export const darkThemeBG = '#2B2B2B';
 export const brandColor = '#FFFF49';
 export const titleColor = '#F5F5F5';
 export const paragraphColor = '#ADAAB1';
-export const buttonColor = '#EF8B50';
-export const buttonColorHover = '#EA6A25';
 export const cardColor = '#191919';
