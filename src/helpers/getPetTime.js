afb5a04118e51a21bc7e706144f88b30795e8702
Hector Herrera <hectorsum81@hotmail.com>
2021-01-07 16:27:53 -0500

    warning fixed

--- src/helpers/getPetTime.js
+++ src/helpers/getPetTime.js
@@ -3,14 +3,13 @@ const moment = require('moment-timezone');
 // * In this file we convert UTF Timezone to PET Timezone, to get our actual hour (Peru)
 export const getPetTime = (data) =>{
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  const elements = data.map(({commit:{committer:{date}}}) => {
-    let date_then = new Date(date);
-    let parseDate = moment(date_then.toUTCString());
-    let parsed = parseDate.tz('America/Lima').format();
-    let parse_completed = new Date(parsed);
-    const date_parsed = `${months[parse_completed.getMonth()]}, ${(parse_completed.getDate() < 10 ? '0' : '') + parse_completed.getDate()} ${parse_completed.getFullYear()}`
-    const time = `${parse_completed.getHours()}:${(parse_completed.getMinutes() < 10 ? '0' : '') + parse_completed.getMinutes()}:${parse_completed.getSeconds()}`
-    return [date_parsed,time];
-  })
-  return elements;
+  const format_timestamp = "MM/dd/yy HH:mm:ss";
+  let date_then = new Date(data);
+  let new_date = date_then.getDate() + '/' + (date_then.getMonth()+1) + '/' + date_then.getFullYear() + " " + date_then.getHours()+":"+date_then.getMinutes()+":"+date_then.getSeconds();
+  let parseDate = moment(new_date,format_timestamp);
+  let parsed = parseDate.tz('America/Lima').format();
+  let parse_completed = new Date(parsed);
+  const date_parsed = `${months[parse_completed.getMonth()]}, ${(parse_completed.getDate() < 10 ? '0' : '') + parse_completed.getDate()} ${parse_completed.getFullYear()}`
+  const time = `${parse_completed.getHours()}:${(parse_completed.getMinutes() < 10 ? '0' : '') + parse_completed.getMinutes()}:${(parse_completed.getSeconds() < 10 ? '0' : '') + parse_completed.getSeconds()}`
+  return [date_parsed,time];
 }
