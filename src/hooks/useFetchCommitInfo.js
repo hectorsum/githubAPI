3fca666de83f127c421bdcfe07dd6574e56b12ab
Hector Herrera <hectorsum81@hotmail.com>
2021-01-07 19:23:21 -0500

    handling errors

--- src/hooks/useFetchCommitInfo.js
+++ src/hooks/useFetchCommitInfo.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import {getCommitInfo} from '../helpers/getCommitInfo';
+
 export const useFetchCommitInfo = (id) => {
   const [info, setInfo] = useState({info:[],
                                     loading:true});
+
   useEffect(()=>{
+    let mounted = true;
     getCommitInfo(id)
     .then(({data:{author:{avatar_url}},
             data:{commit:{author,message}},
@@ -11,11 +14,15 @@ export const useFetchCommitInfo = (id) => {
                   committer:{date}}},
             data:{html_url}
           }) => {
-        setInfo({
-          info: {avatar_url,date,author,message,name,email,html_url},
-          loading:false
-        })
+        if(mounted){
+          setInfo({
+            info: {avatar_url,date,author,message,name,email,html_url},
+            loading:false
+          })
+        }
     })
+    .catch(err => setInfo({info:false,loading:false}));
+    return () => mounted = false;
   },[id]);
   return info;
 }
